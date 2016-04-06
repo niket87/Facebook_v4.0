561590bbc3dec4d38515d773d8b2c14d5f0b6e05
Niket Singh <singh.niket@gmail.com>
2016-04-07 01:36:47 +0530

    Integration within Activity (not in Fragment). Fragment is recommended but.

--- app/src/main/java/com/facebook_v4/MainActivity.java
+++ app/src/main/java/com/facebook_v4/MainActivity.java
@@ -1,16 +1,129 @@
 package com.facebook_v4;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private TextView text;
+    private CallbackManager mCallbackManager;
+
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null) {
+                text.setText("Welcome " + profile.getName());
+            } else {
+                text.setText("profile is null");
+            }
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    };
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //comment
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+        //LoginManager.getInstance().registerCallback(mCallbackManager, mCallback);
+
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        text = (TextView) findViewById(R.id.text_details);
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(mCallbackManager, mCallback);
+
+
+
+
+        /////////////////// Add code to print out the key hash//////////////////
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    //"com.facebook_v4",   ////// specify your packageName
+                    getApplicationContext().getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("KeyHash:", "NameNotFoundException " + e);
+            Log.d("KeyHash:", "NameNotFoundException ENDS");
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.d("KeyHash:", "NoSuchAlgorithmException");
+
+        }
+        ////////////////////////////////////////////////////////////////////////////
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+            text.setText("Welcome bro " + data.getExtras().toString());
+
+
+
+        if(Profile.getCurrentProfile() == null) {
+            mProfileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                    // profile2 is the new profile
+                    Log.v("facebook - profile", profile2.getFirstName());
+                    text.setText("Welcome22 " + profile2.getName());
+                    mProfileTracker.stopTracking();
+                }
+            };
+            mProfileTracker.startTracking();
+        }
+        else {
+            Profile profile = Profile.getCurrentProfile();
+            Log.v("facebook - profile", profile.getFirstName());
+        }
 
     }
 }
